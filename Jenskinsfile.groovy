d2f97a0cda97e0dced6a3d229609db79fae5756d
Timothy Gentet-O'Brien <timothygentetobrien@gmail.com>
2017-04-13 16:02:40 +0100

    Create Jenskinsfile.groovy

---
+++ Jenskinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+  agent any
+  
+  stages {
+    stage('Build') {
+      steps {
+        echo 'Builing...'
+      }
+    }
+    
+    stage('Test') {
+      steps {
+        echo 'Testing...'
+      }
+    }
+    
+    stage('Deploy') {
+      steps {
+        echo 'Deploying...'
+      }
+    }
+  }
+}
